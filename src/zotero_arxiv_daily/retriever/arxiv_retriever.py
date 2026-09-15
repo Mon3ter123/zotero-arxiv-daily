0925c6e391b819a6ae696fc3617e461d9c51191a
Mon3ter123 <sunyixin22@mails.tsinghua.edu.cn>
2026-09-15 13:50:37 +0800

    Update arxiv_retriever.py

--- src/zotero_arxiv_daily/retriever/arxiv_retriever.py
+++ src/zotero_arxiv_daily/retriever/arxiv_retriever.py
@@ -113,7 +113,7 @@ class ArxivRetriever(BaseRetriever):
         if self.config.source.arxiv.category is None:
             raise ValueError("category must be specified for arxiv.")
 
-      def _retrieve_raw_papers(self) -> list[ArxivResult]:
+    def _retrieve_raw_papers(self) -> list[ArxivResult]:
         client = arxiv.Client(num_retries=2, delay_seconds=10)
 
         query = '+'.join(self.config.source.arxiv.category)
@@ -275,3 +275,4 @@ def extract_text_from_tar(paper: ArxivResult) -> str | None:
         operation="Tar extraction",
         paper_title=paper.title,
     )
+
